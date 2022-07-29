6c75dec06b95f458425ad800bbe2e1e5e486f80a
Lucas Antevere Santana <39019778+lucasasantana@users.noreply.github.com>
2022-07-29 17:59:26 -0300

    Initial Commit

---
+++ JobsityApplication/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  JobsityApplication
+//
+//  Created by Lucas Antevere Santana on 29/07/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
